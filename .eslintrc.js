bd5e340af0ba074acbb98d096e06fb80d3172076
Wiecek-K <krystiandwiecek@gmail.com>
2023-07-02 23:39:35 +0200

    added ts-node-dev
    now "npm run dev"  start app.ts in on go mode

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,12 @@
 module.exports = {
   parser: "@typescript-eslint/parser",
-  extends: ["plugin:@typescript-eslint/recommended"],
+  extends: ["plugin:@typescript-eslint/recommended", "plugin:node/recommended"],
   parserOptions: {
     ecmaVersion: 2018,
     sourceType: "module",
   },
   rules: {
-   quotes: ["error", "double"],
+    quotes: ["error", "double"],
     "@typescript-eslint/explicit-function-return-type": "off",
     "@typescript-eslint/no-explicit-any": 1,
     "@typescript-eslint/no-inferrable-types": [
