f9a35e9fac198d49edb17dbe388a746fa8905688
Wiecek-K <krystiandwiecek@gmail.com>
2023-07-05 15:27:45 +0200

    fixed CR issues

--- src/controllers/flashcardsController.ts
+++ src/controllers/flashcardsController.ts
@@ -7,18 +7,18 @@ import asyncHandler from "express-async-handler"
 export const createFlashcard = asyncHandler(
   async (req: Request, res: Response) => {
     if (!req.body.front || !req.body.back) {
-      res.status(400)
-      throw new Error(
-        "To add a card, you need the texts on the front and back sides"
-      )
+      res
+        .status(400)
+        .send("To add a card, you need the texts on the front and back sides")
+      return
     }
     const isExisting = await Flascard.findOne({ front: req.body.front })
 
     if (isExisting) {
-      res.status(400)
-      throw new Error(
-        "You can't duplicate flaschcards, please change the front text"
-      )
+      res
+        .status(400)
+        .send("You can't duplicate flaschcards, please change the front text")
+      return
     }
     const flashcard = await Flascard.create({
       front: req.body.front,
