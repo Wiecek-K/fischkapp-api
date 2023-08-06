dd3ec546ceac7dbb56dbe46e0c772e198e70ae3f
Wiecek-K <krystiandwiecek@gmail.com>
2023-08-06 18:25:46 +0200

    npm i typescript
    ts-node
    
    Update package.json
    
    Update package.json
    
    Update package.json
    
    Update package.json
    
    fix comma
    
    Update package.json
    
    Update package.json
    
    Update package.json
    
    Update package.json
    
    Update package.json
    
    fix ts errors
    
    Update tsconfig.json

--- src/middlewares/errors.ts
+++ src/middlewares/errors.ts
@@ -2,12 +2,15 @@ import { ErrorRequestHandler, Request, Response, NextFunction } from "express"
 
 export const notFound = (req: Request, res: Response, next: NextFunction) => {
   const err = new Error("404 page not found")
-  //   err.status = 404
   next(err)
 }
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
+export const errorHandler: ErrorRequestHandler = (
+  err: Error,
+  req: Request,
+  res: Response
+) => {
   const statusCode = res.statusCode ? res.statusCode : 500
 
   res.status(statusCode)
